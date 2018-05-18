bad814f0b4f46d4ce5a8198d50974241a9a90f18
hasnain@w3engineers.com <hasnain@w3engineers.com>
2018-05-18 17:11:15 +0600

    initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public int damage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player" && other.tag != "Enemy")
+        {
+            Debug.Log("Handle Tag: " + other.tag);
+            return;
+        }
+
+        Destroy(gameObject);
+
+        Health health = other.GetComponent<Health>();
+        if (health  != null)
+        {
+            health.TakeDamage(damage);
+
+            // if(health.GetCurrentHealth() == 0)
+            // {
+            //     Destroy(other.gameObject);
+            // }
+        }
+        else
+        {
+            Debug.Log("Health Script Not Found!");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletFab;
+    public Transform bulletSpawn;
+    public float bulletSpeed;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        var x = Input.acceleration.x * 150.0f;
+        var z = Input.acceleration.y * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     CmdFire();
+        // }
+
+        if (Input.touchCount>0)
+        {
+            CmdFire();
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        //GetComponent<Renderer>().tag = "Hero";
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        var bullet = (GameObject)Instantiate (
+            bulletFab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+        
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
+}
